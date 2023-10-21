b485308cf1a89d2f251bf48828ef986160b4f975
LNS2905 <nhatsang2905@gmail.com>
2023-10-21 13:51:50 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 echo "Building app..."
 npm run build
 echo "Deploy files to server..."
-scp -r -i ~laundyhubvn.com dist/* root@143.198.196.146:/var/www/laundryhubvn/
+scp -r -i "C:\Users\nhats\laundryhubvn.com" build/* root@143.198.196.146:/var/www/laundryhubvn/
 echo "Done!"
