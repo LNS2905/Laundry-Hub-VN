6ee34e89ace0abf716c5cc73721591a18507a298
LNS2905 <nhatsang2905@gmail.com>
2023-11-01 16:20:00 +0700

    update fix bug logout

--- src/components/Dropdowns/CustomerDropdown.js
+++ src/components/Dropdowns/CustomerDropdown.js
@@ -66,8 +66,7 @@ const UserDropdown = () => {
         <button
           className="text-sm py-2 px-4 font-normal block w-full whitespace-nowrap bg-transparent text-blueGray-700"
           onClick={() => {
-            localStorage.removeItem("token");
-            localStorage.removeItem("role");
+            localStorage.removeItem('account');
             navigate.push("/");
           }}
         >
